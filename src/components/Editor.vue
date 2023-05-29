91a44eb491afd703707cada7d700f0cc9019b550
曹佳豪 <cao1395654040@hotmail.com>
2023-05-29 09:56:22 +0800

    feat: improve ui design.

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+import { useWindowSize } from '@vueuse/core'
 import { ref } from 'vue'
 
 const expand = ref<boolean>(false)
 
+const { width } = useWindowSize()
+
+/**
+ * 展开编辑器
+ */
 function onExpandEditor() {
   expand.value = true
 }
 
+/**
+ * 隐藏编辑器
+ */
 function onCloseEditor() {
   expand.value = false
 }
@@ -14,27 +23,39 @@ function onCloseEditor() {
 
 <template>
   <div
-    :class="[
-      expand === true ? 'h-240px' : 'h-79px',
-    ]" class="color-base transition-all"
-    b="0 t-1 solid gray-200 dark:dark-200"
+    class="color-base transition-all" b="0 t-1 solid gray-200 dark:dark-200"
+    :class="expand === true ? 'h-240px' : 'h-79px'"
   >
-    <div class="flex flex-rows p-24px" :class="expand === true ? 'h-191px' : 'h-31px'">
-      <div class="w-15%" />
+    <div
+      class="flex flex-row p-24px" :class="[
+        expand === true ? 'h-191px' : 'h-31px',
+        expand === true ? 'bg-#f1f1f50 dark:bg-dark' : 'bg-base',
+      ]"
+    >
+      <div v-if="width >= 1000" class="w-15%" />
       <div class="flex-1 flex flex-col">
-        <div v-if="expand === false" class="flex-1" />
-        <input
-          class="bg-base b-0 outline-none color-base block-words" placeholder="Enter Something..." @focus="onExpandEditor"
-          @blur="onCloseEditor"
-        >
+        <div :class="expand === true ? 'h-0' : 'flex-1'" />
+        <textarea
+          overflow="x-hidden y-scroll"
+          placeholder="Enter Something..."
+          class="bg-transparent b-0 outline-none color-base text-4 h-100% p-0 m-0"
+          :style="{ lineHeight: `${expand === true ? '24px' : '31px'}` }"
+          @focus="onExpandEditor" @blur="onCloseEditor"
+        />
         <div class="flex-1" />
       </div>
       <div class="flex flex-col">
         <div class="flex-1" />
         <div class="h-31px w-31px icon-button i-carbon-send-alt" />
-        <div v-if="expand === false" class="flex-1" />
+        <div :class="expand === true ? 'h-0' : 'flex-1'" />
       </div>
-      <div class="w-15%" />
+      <div v-if="width >= 1000" class="w-15%" />
     </div>
   </div>
 </template>
+
+<style scoped>
+textarea::-webkit-scrollbar {
+  display: none;
+}
+</style>
