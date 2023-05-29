7040dee9bf28d886027d68b984c44d0503845a74
曹佳豪 <cao1395654040@hotmail.com>
2023-05-29 09:57:14 +0800

    feat: set home page overflow hidden

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@ import SettingSlideBar from '@/components/SettingSlideBar.vue'
 </script>
 
 <template>
-  <main class="flex flex-row h-100vh bg-base">
+  <main class="flex flex-row h-100vh bg-base overflow-hidden">
     <LeftSideBar />
     <div flex="1" h="100%" class="flex flex-col">
       <div class="flex-1" />
