5a6634065f04d5d64e46b5f4e23788442ec40c76
曹佳豪 <cao1395654040@hotmail.com>
2023-05-30 15:11:19 +0800

    feat: support the render session description on the body header.

--- src/components/BodyHeader.vue
+++ src/components/BodyHeader.vue
@@ -12,16 +12,23 @@ function onOpenLeftSideBar() {
 </script>
 
 <template>
-  <div class="h-79px flex flex-row text-6 color-base border-base overflow-hidden" b="0 b-1 solid" p="x-24px">
+  <div class="h-79px flex flex-row text-6 color-base border-base overflow-hidden gap-2" b="0 b-1 solid" p="x-24px">
     <div v-if="leftSideBarStore.expand === false" class="flex flex-col m-r-4">
       <div class="flex-1" />
       <div class="icon-button i-carbon-menu" @click="onOpenLeftSideBar" />
       <div class="flex-1" />
     </div>
-    <div class="flex-1 flex flex-col">
+    <div class="flex-1 flex flex-col overflow-hidden">
       <div class="flex-1" />
       <div>
-        {{ conversationStore.conversationInfo?.name }}<div class="flex-1" />
+        {{ conversationStore.conversationInfo?.name }}
+        <div class="flex-1" />
+      </div>
+      <div
+        v-if="conversationStore.conversationInfo?.description && conversationStore.conversationInfo?.description!.trim().length > 0"
+        class="text-3 color-fade m-t1 overflow-hidden" style="text-overflow: ellipsis;white-space: nowrap;"
+      >
+        {{ conversationStore.conversationInfo?.description! }}
       </div>
       <div class="flex-1" />
     </div>
