0970f111ad8c00fb0327242e1e728f1e11361927
曹佳豪 <cao1395654040@hotmail.com>
2023-08-05 10:57:20 +0800

    feat: update dialog to open animation

--- src/ui/Dialog.vue
+++ src/ui/Dialog.vue
@@ -48,29 +48,43 @@ function closeModal() {
 </script>
 
 <template>
-  <div
-    ref="dialogRoot" class="flex flex-row h-100vh w-screen left-0 top-0 color-base backdrop-blur-3"
-    :class="open === true ? 'fixed' : 'none'"
-  >
-    <div class="flex-1 hide-view" />
-    <div class="flex flex-col">
+  <Transition>
+    <div
+      v-if="open"
+      ref="dialogRoot" class="flex flex-row h-100vh w-screen left-0 top-0 color-base backdrop-blur-3 fixed"
+    >
       <div class="flex-1 hide-view" />
-      <div
-        class="bg-base border-base shadow-2xl" b="1 rd-1 solid"
-        :class="width >= 1200 ? 'w-600px' : width >= 800 && width < 1200 ? 'w-500px' : 'w-300px'"
-      >
-        <div class="flex flex-row p-4 border-base" b="0 b-1 solid">
-          <div class="flex-1 text-5 font-bold">
-            {{ props.title }}
+      <div class="flex flex-col">
+        <div class="flex-1 hide-view" />
+        <div
+          class="bg-base border-base shadow-2xl" b="1 rd-1 solid"
+          :class="width >= 1200 ? 'w-600px' : width >= 800 && width < 1200 ? 'w-500px' : 'w-300px'"
+        >
+          <div class="flex flex-row p-4 border-base" b="0 b-1 solid">
+            <div class="flex-1 text-5 font-bold">
+              {{ props.title }}
+            </div>
+            <div data-cursor="block" icon-button class="i-carbon-close text-5 h-24px line-height-24px" @click="emits('onClose')" />
           </div>
-          <div data-cursor="block" icon-button class="i-carbon-close text-5 h-24px line-height-24px" @click="emits('onClose')" />
+          <div class="p-4">
+            <slot />
+          </div>
         </div>
-        <div class="p-4">
-          <slot />
-        </div>
+        <div class="flex-1 hide-view" />
       </div>
       <div class="flex-1 hide-view" />
     </div>
-    <div class="flex-1 hide-view" />
-  </div>
+  </Transition>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
