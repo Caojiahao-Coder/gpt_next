428436a7930ce008821f5352eec66bf1b71fe2bc
曹佳豪 <cao1395654040@hotmail.com>
2023-11-10 21:59:50 +0800

    chore: smalle update

--- src/store/localstorage.ts
+++ src/store/localstorage.ts
@@ -9,3 +9,5 @@ export const speechLang: any = useStorage('speech-lang', 'zh-CN')
 export const speechVoice: any = useStorage('speech-voice', 'alloy')
 
 export const ttsModels: any = useStorage('tts-models', 'tts-1')
+
+export const language: any = useStorage('language', 'auto')
