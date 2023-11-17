ebd88a15c8e1ba83b48374d4a35e9e04810550aa
曹佳豪 <cao1395654040@hotmail.com>
2023-11-17 22:48:04 +0800

    chore: smalle update

--- src/openai/parser.ts
+++ src/openai/parser.ts
@@ -53,33 +53,43 @@ export async function parserStreamText(response: Response, callback: (content: s
     let dataDone = false
     const arr = value.split('\n')
     arr.forEach((data) => {
-      if (data.length === 0)
-        return
-      if (data.startsWith(':'))
-        return
-      if (data === 'data: [DONE]') {
-        dataDone = true
-        return
-      }
-      const json = JSON.parse(data.substring(6)) as MessageResultByStreamType
-      if (json.choices) {
-        const messageItem = json.choices[0].delta.content
-        if (messageItem)
-          callback(messageItem)
-
-        if (json.choices[0].delta.tool_calls && json.choices[0].delta.tool_calls.length > 0) {
-          const toolsCall = json.choices[0].delta.tool_calls[0] as ToolCallInfo
-          if (toolsCall.id)
-            function_calling_info.id = function_calling_info.id + toolsCall.id.replace(function_calling_info.id, '')
-          if (toolsCall.index)
-            function_calling_info.index = Number(function_calling_info.index.toString() + toolsCall.index.toString().replace(function_calling_info.index.toString(), ''))
-          if (toolsCall.type)
-            function_calling_info.type = function_calling_info.type + toolsCall.type.replace(function_calling_info.type, '')
-          if (toolsCall.function.name)
-            function_calling_info.function.name = function_calling_info.function.name + toolsCall.function.name.replace(function_calling_info.function.name, '')
-          if (toolsCall.function.arguments)
-            function_calling_info.function.arguments = (function_calling_info.function.arguments + toolsCall.function.arguments.replace(function_calling_info.function.arguments, ''))
+      try {
+        if (data.length === 0)
+          return
+        if (data.startsWith(':'))
+          return
+        if (data === 'data: [DONE]') {
+          dataDone = true
+          return
         }
+        const json = JSON.parse(data.substring(6)) as MessageResultByStreamType
+        if (json.choices) {
+          const messageItem = json.choices[0].delta.content
+          if (messageItem)
+            callback(messageItem)
+
+          try {
+            if (json.choices[0].delta.tool_calls && json.choices[0].delta.tool_calls.length > 0) {
+              const toolsCall = json.choices[0].delta.tool_calls[0] as ToolCallInfo
+              if (toolsCall.id)
+                function_calling_info.id = function_calling_info.id + toolsCall.id.replace(function_calling_info.id, '')
+              if (toolsCall.index)
+                function_calling_info.index = Number(function_calling_info.index.toString() + toolsCall.index.toString().replace(function_calling_info.index.toString(), ''))
+              if (toolsCall.type)
+                function_calling_info.type = function_calling_info.type + toolsCall.type.replace(function_calling_info.type, '')
+              if (toolsCall.function.name)
+                function_calling_info.function.name = function_calling_info.function.name + toolsCall.function.name.replace(function_calling_info.function.name, '')
+              if (toolsCall.function.arguments)
+                function_calling_info.function.arguments = (function_calling_info.function.arguments + toolsCall.function.arguments.replace(function_calling_info.function.arguments, ''))
+            }
+          }
+          catch {
+            // ignore
+          }
+        }
+      }
+      catch {
+        // ignore
       }
     })
     if (dataDone)
